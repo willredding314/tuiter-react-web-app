ab2c42a9c5edf3919e292c7bd97243b15e218737
Will Redding <willredding3.14@gmail.com>
2022-11-28 22:16:09 -0500

    a9 2

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-//const TUITS_API = 'https://will-tuiter-node-server.herokuapp.com/api/tuits';
-const TUITS_API = 'http://localhost:4000/api/tuits'
+const TUITS_API = 'https://will-tuiter-node-server.herokuapp.com/api/tuits';
+//const TUITS_API = 'http://localhost:4000/api/tuits'
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
     return response.data;
